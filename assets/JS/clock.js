7845f4f18fc5f095d0ae39b417d448f563f97d19
BransonJ <joshua.branson@lrs.com>
2023-03-28 12:30:34 -0500

    readable time function working

--- assets/JS/clock.js
+++ assets/JS/clock.js
@@ -1,6 +1,37 @@
 function userTime() {
     let today = new Date();
-    let currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let currentHour = today.getHours();
+    let currentMinutes = today.getMinutes();
+    let currentSeconds = today.getSeconds();
+    let currentTime = currentHour + ":" + currentMinutes + ":" + currentSeconds;
+
+    function readableHour() {
+        if (currentHour <= 9) {
+            return readableHour = ("0" + currentHour);
+        } else {
+            return readableHour = currentHour;
+        }
+    }
+
+    function readableMinutes() {
+        if (currentMinutes <= 9) {
+            return readableMinutes = ("0" + currentMinutes);
+        } else {
+            return readableMinutes = currentMinutes;
+        }
+    }
+
+    function readableSeconds() {
+        if (currentSeconds <= 9) {
+            return readableSeconds = ("0" + currentSeconds);
+        } else {
+            return readableSeconds = currentSeconds;
+        }
+    }
+
+    console.log(readableMinutes());
+    console.log(readableHour());
+    console.log(readableSeconds());
     $('div.clockData').html(currentTime);
 }
 
